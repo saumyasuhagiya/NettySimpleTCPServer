5d822799525d3902bbfd22cc5be77e02b84e2c28
Saumya Suhagiya <saumya.patel2001@gmail.com>
2023-04-25 11:43:21 -0500

    Added continuous loop to generate data stream

--- src/main/java/netty/tcp/server/Main.java
+++ src/main/java/netty/tcp/server/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             SimpleNettyServerBootstrap simpleNettyServerBootstrap = new SimpleNettyServerBootstrap();
-            simpleNettyServerBootstrap.start(8080);
+            simpleNettyServerBootstrap.start(8888);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
